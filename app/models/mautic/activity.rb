965b367a8c099af22112a00b75535d4de35f4c10
ozasuke <oz.boc.l.3t.0409w@gmail.com>
2019-01-23 14:14:44 +0900

    fix all and add email_sent_all

--- app/models/mautic/activity.rb
+++ app/models/mautic/activity.rb
@@ -8,6 +8,16 @@ module Mautic
       def field_name
         'events' 
       end
+
+      def all(connection, params = {})
+        json = connection.request(:get, "api/#{endpoint}", params: params)
+        json[field_name].map { |j| self.new(connection, j) }
+      end
+
+      def email_sent_all(connection, params = {})
+        email_sent_params = {filters: {includeEvents: ['email.sent']}}
+        all(connection, params.merge(email_sent_params))
+      end
     end
   end
 end
