ed212d090b430bd1e70781c917417ff688aea312
TaKO8Ki <takoyaki0316@gmail.com>
2019-02-24 15:34:42 +0900

    get_segment_membershipsメソッドの修正

--- app/models/mautic/contact.rb
+++ app/models/mautic/contact.rb
@@ -45,7 +45,7 @@ module Mautic
       doNotContact.present?
     end
 
-    def get_segment_memberships(contact)
+    def self.get_segment_memberships(contact)
       contact_id = contact.is_a?(Mautic::Contact) ? contact.id : contact
       segments = @connection.request(:get, %(api/contacts/#{contact_id}/segments))["lists"].values
       return segments
