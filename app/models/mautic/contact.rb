7205b7db407b3a57f76419fe56dab4a6ab9e306b
ozasuke <oz.boc.l.3t.0409w@gmail.com>
2020-05-04 15:44:46 +0900

    add dnc methods

--- app/models/mautic/contact.rb
+++ app/models/mautic/contact.rb
@@ -19,32 +19,61 @@ module Mautic
       } if source
     end
 
-    def add_dnc(comments: '')
-      begin
-        json = @connection.request(:post, "api/contacts/#{id}/dnc/email/add", { body: {comments: comments} })
-        clear_changes
-      rescue ValidationError => e
-        self.errors = e.errors
-      end
+    # @!group Do Not Contact
+    # @see https://developer.mautic.org/#add-do-not-contact
 
-      self.errors.blank?
-    end
-
-    def remove_dnc
-      begin
-        json = @connection.request(:post, "api/contacts/#{id}/dnc/email/remove", { body: {} })
-        clear_changes
-      rescue ValidationError => e
-        self.errors = e.errors
-      end
-
-      self.errors.blank?
-    end
-
-    def dnc?
+    def do_not_contact?
       doNotContact.present?
     end
+    alias dnc? do_not_contact?
 
+    # @return [Array[Hash]]
+    def do_not_contact
+      return unless do_not_contact?
+
+      # Based on mautic docs => Contacts constants: Contacts::UNSUBSCRIBED (1), Contacts::BOUNCED (2), Contacts::MANUAL (3)
+      reason_list = { 1 => :unsubscribed, 2 => :bounced, 3 => :manual }
+      @do_not_contact ||= doNotContact.collect do |hsh|
+        { reason_list[hsh["reason"]] => hsh["comments"] }
+      end
+    end
+
+    def bounced?
+      do_not_contact? && !!do_not_contact.detect { |dnc| dnc.key?(:bounced) }
+    end
+
+    def unsubscribed?
+      do_not_contact? && !!do_not_contact.detect { |dnc| dnc.key?(:unsubscribed) }
+    end
+
+    def do_not_contact!(comments: '')
+      begin
+        json = @connection.request(:post, "api/contacts/#{id}/dnc/email/add", body: { comments: comments })
+        self.attributes = { doNotContact: json[endpoint.singularize]["doNotContact"] }
+        clear_changes
+      rescue ValidationError => e
+        self.errors = e.errors
+      end
+
+      self.errors.blank?
+    end
+    alias add_dnc do_not_contact!
+
+    def remove_do_not_contact!
+      begin
+        json = @connection.request(:post, "api/contacts/#{id}/dnc/email/remove", body: {})
+        self.attributes = { doNotContact: json[endpoint.singularize]["doNotContact"] }
+        clear_changes
+      rescue ValidationError => e
+        self.errors = e.errors
+      end
+
+      self.errors.blank?
+    end
+    alias remove_dnc remove_do_not_contact!
+
+    # !endgroup
+    
     def self.get_segment_memberships(connection: nil, contact: nil)
       contact_id = contact.is_a?(Mautic::Contact) ? contact.id : contact
       segments = connection.request(:get, %(api/contacts/#{contact_id}/segments))["lists"].values
@@ -55,5 +84,11 @@ module Mautic
       @connection.request(:get, %(api/contacts/#{id}/segments))["lists"].values
     end
 
+    private
+    
+    def clear_change
+      super
+      remove_instance_variable :@do_not_contact
+    end
   end
 end
