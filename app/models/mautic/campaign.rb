317cd0d06efa9aebcf76b25007f57a0726562da8
Michiharu Ono <michiharuono77@gmail.com>
2023-10-31 17:17:04 +0900

    circular argument reference errorの対処

--- app/models/mautic/campaign.rb
+++ app/models/mautic/campaign.rb
@@ -1,7 +1,7 @@
 module Mautic
   class Campaign < ApplicationRecord
 
-    def self.list_campaign_contacts(connection: connection, campaign: campaign)
+    def self.list_campaign_contacts(connection:, campaign:)
       contacts = connection.request :get, %(api/campaigns/#{campaign}/contacts)
       return contacts["contacts"]
     end
